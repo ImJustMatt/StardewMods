e0ce48046d3fb6629b71be701c9f0ce9a8a0ae75
Matthew <justmatt@outlook.com>
2021-03-06 02:15:18 -0600

    Enumerable Shuffler

--- Common/Extensions/CommonExtensions.cs
+++ Common/Extensions/CommonExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ImJustMatt.Common.Extensions
 {
@@ -7,6 +9,33 @@ namespace ImJustMatt.Common.Extensions
         public static int RoundUp(this int i, int d = 1)
         {
             return (int) (d * Math.Ceiling((float) i / d));
+        }
+
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+        {
+            return source.Shuffle(new Random());
+        }
+
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, Random rng)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (rng == null)
+                throw new ArgumentNullException(nameof(rng));
+            return source.ShuffleIterator(rng);
+        }
+
+        private static IEnumerable<T> ShuffleIterator<T>(
+            this IEnumerable<T> source,
+            Random rng)
+        {
+            var buffer = source.ToList();
+            for (var i = 0; i < buffer.Count; i++)
+            {
+                var j = rng.Next(i, buffer.Count);
+                yield return buffer[j];
+                buffer[j] = buffer[i];
+            }
         }
     }
 }
